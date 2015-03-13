297fef797d207024f5703f3aaa214e5ead507b8b
oranthony <anthonyloroscio@gmail.com>
2015-03-13 22:03:59 +0100

    push

--- Javascript/js/recherche.js
+++ Javascript/js/recherche.js
@@ -59,6 +59,8 @@ $(document).ready(function(){
 
 });
 
+
+
 //search for drink
 function SearchDrinkbyName(data){
     var drink = $(this).serialize(); 
@@ -87,14 +89,14 @@ function OnSuccessName(json){
 
 //search by ingedrient
 function SearchDrinkbyIngredient(data){
-    var drink = $(this).serialize(); 
-    var drink = $('#term').val();
-    $(this).find('input').prop('disable', true);
-    $(this).fadeOut('fast');
-    $('#result').html("<h2 class='loading'>Your cocktail is on its way!</h2>");
+    //var drink = $(this).serialize(); 
+    //var drink = $('#term').val();
+    //$(this).find('input').prop('disable', true);
+    //$(this).fadeOut('fast');
+    //$('#result').html("<h2 class='loading'>Your cocktail is on its way!</h2>");
     $.ajax({
         type: 'GET',
-        url: "http://addb.absolutdrinks.com/drinks/with/" + drink + "/?apiKey=c6d792879d4b44119788eefc6748393a",
+        url: "http://addb.absolutdrinks.com/drinks/with/apple-liqueur/lemonade/?apiKey=c6d792879d4b44119788eefc6748393a",
         data: 'json',
         success: OnSuccessDrinkbyIngredient,
         error: function(xhr, error){
@@ -104,12 +106,41 @@ function SearchDrinkbyIngredient(data){
     return false;
 }
 
-function OnSuccessDrinkbyIngredient(json){
-  $('#result').html('<h2 class="loading">Cocktail found </h2>');
-  $('#name').html('<p><b>Name</b> : ' + JSON.stringify(json.result[0].name) + '</p>');    
-  $('#description').html('<p><b>Description</b> : ' + JSON.stringify(json.result[0].description) + '</p>')
-}
 
+//******************
+
+function OnSuccessDrinkbyIngredient(drinkfound){
+  //$('#result').html('<h2 class="loading">Cocktail found </h2>');
+  //$('#name').html('<p>' + JSON.stringify(drinkfound.result[0].name) + '</p>');    
+  //$('#description').html('<p><b>Description</b> : ' + JSON.stringify(json.result[0].description) + '</p>')
+  var url;
+  
+  //$('#description').html('<img src=' + url +' alt="drink" style="width:200px; height:200px;" /> ')
+  
+  /*$('#result').html('<div class ="row">
+                        <div class="col-sm-1"></div>
+                        <div class="col-sm-2"><img src=' + url +' alt="drink" style="width:200px; height:200px;" /><p>' + JSON.stringify(drinkfound.result[0].name) + '</p></div>
+                    </div>')*/
+
+
+  $('#result').append(
+    $('<div />').addClass('row')
+  )
+  console.log(drinkfound);
+  $.each(drinkfound.result, function (i, item) {
+    console.log(drinkfound.result[i].id);
+    url = decodeURI('http://assets.absolutdrinks.com/drinks/transparent-background-white/300x400/'+drinkfound.result[i].id+'.png');
+    $('#result').append(
+      $('<div />').addClass('col-sm-2 text-center')
+        .append('<img src=' + url +' alt="drink" style="width:200px; height:200px;" /><p>' + JSON.stringify(drinkfound.result[i].name) + '</p>')
+          /*$('<img />')
+          .attr('src', url)
+          .attr('alt', 'drink')
+          .attr('style', 'width:200px; height:200px;')*/
+    )
+  })
+}
+//*******************
 
 //search Ingredient
 function SearchIngredient(data){
@@ -224,25 +255,28 @@ function OnSuccesstabIngredient(json){
   $.each(json, function (key, data) {
     $.each(data, function (index, data) {
       if(data.isBaseSpirit == true || data.type == 'spirits-other')
-        $('#base-spirit-table').append('<tr><td>' + JSON.stringify(data.name) + '</td></tr>');
+        $('#base-spirit-table').append('<tr><td><a href='+JSON.stringify(data.id)+'>' + JSON.stringify(data.name) + '</a></td></tr>');
     })
   })
+ 
+
 
   //mixers"type":"mixers"
   $('#Mixers-table').html('<thead><tr><th>Mixers</th></tr></thead>');
   $.each(json, function (key, data) {
     $.each(data, function (index, data) {
       if(data.type == 'mixers')
-        $('#Mixers-table').append('<tr><td>' + JSON.stringify(data.name) + '</td></tr>');
+        $('#Mixers-table').append('<tr><td><a href='+JSON.stringify(data.id)+'>' + JSON.stringify(data.name) + '</a></td></tr>');
     })
   })
 
   //fruit
   $('#Fruit-table').html('<thead><tr><th>Fruits</th></tr></thead>');
   $.each(json, function (key, data) {
+
     $.each(data, function (index, data) {
       if(data.type == 'fruits')
-        $('#Fruit-table').append('<tr><td>' + JSON.stringify(data.name) + '</td></tr>');
+        $('#Fruit-table').append('<tr><td><a href='+JSON.stringify(data.id)+'>' + JSON.stringify(data.name) + '</a></td></tr>');
     })
   })
 
@@ -251,17 +285,109 @@ function OnSuccesstabIngredient(json){
   $.each(json, function (key, data) {
     $.each(data, function (index, data) {
       if(data.type == 'others')
-        $('#Ingredients-table').append('<tr><td>' + JSON.stringify(data.name) + '</td></tr>');
+        $('#Ingredients-table').append('<tr><td><a href='+JSON.stringify(data.id)+'>' + JSON.stringify(data.name) + '</a></td></tr>');
     })
   })
   
 }
 
 
+//function create tag
+function createTag(nameTag) {
+
+}
+
+
+$("#myTags").on('click', '.tag-close', function() {
+  $(this).parent().fadeOut();
+
+})
 
 
 
 
+
+//get the id of the clicked object
+var ingredientQuery =[];
+$('body').on('click', '#base-spirit-table a, #Mixers-table a, #Fruit-table a, #Ingredients-table a', function(event) {
+  event.preventDefault();
+        var href = $(this).text();
+        console.log('this');
+        console.log(this);
+        console.log('href:');
+        console.log(href);
+        //var name = $(this).closest('td').text();
+        //console.log(name);
+        var currText = href;
+        //<button type='button' class='close' data-dismiss='alert'>lemonade X</button>
+        //$(".searchbar").append('<input type="text" value='+ $currText +' data-role="tagsinput"/>')
+        //$("#myTags").append('<li>' + $currText + '</li>')
+        /*$("#myTags").tagit();
+        $("#myTags").tagit("createTag", $currText);
+        //$("#myTags").tagit($currText);
+        //$("#myTags").data($currText);*/
+        $("#myTags").append('<span id="tag">' + currText + '<span class="glyphicon glyphicon-record tag-close"></span></span>')
+
+
+        if(href) {
+            
+            ingredientQuery.push(href);
+        }
+        console.log(ingredientQuery);
+        console.log(ingredientQuery.length);
+        //console.log(ingredientQuery);
+
+        if(ingredientQuery.length >= 1){
+          var querry = '';
+          for(i = 0; i < ingredientQuery.length; ++i){
+            querry += ingredientQuery[i] + '/';
+          }
+          console.log(ingredientQuery[0]);
+          console.log(querry);
+           $.ajax({
+            type: 'GET',
+            url: "http://addb.absolutdrinks.com/drinks/with/" + querry + "?apiKey=c6d792879d4b44119788eefc6748393a",
+            data: 'drinkfound',
+            success: OnSuccessDrinkbyIngredient,
+            error: function(xhr, error){
+              alert(xhr.responseText + ' ' + error + ' ' + xhr.status);
+            }        
+          });
+        }
+        /*if(ingredientQuery) {
+          $.ajax({
+          type: 'GET',
+          url: "http://addb.absolutdrinks.com/drinks/with/" + drink + "/?apiKey=c6d792879d4b44119788eefc6748393a",
+          data: 'json',
+          success: OnSuccessDrinkbyIngredient,
+          error: function(xhr, error){
+            alert(xhr.responseText + ' ' + error + ' ' + xhr.status);
+          }        
+    });
+    return false;
+        }*/
+    });
+
+
+/*$(document).ready(function(){
+  if(ingredientQuery.length > 1) { 
+    //construct param of the url
+    console.log("coucou");
+
+    //search by ingedrient
+      //$(this).find('input').prop('disable', true);
+      $.ajax({
+          type: 'GET',
+          url: "http://addb.absolutdrinks.com/drinks/with/apple-liqueur/lemonade/?apiKey=c6d792879d4b44119788eefc6748393a",
+          data: 'json',
+          success: OnSuccessDrinkbyIngredient,
+          error: function(xhr, error){
+            alert(xhr.responseText + ' ' + error + ' ' + xhr.status);
+          }        
+      });
+      
+  }
+}*/
 
 
 
